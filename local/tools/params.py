71744795bdf8b024b662d15278b4304d6afdb67f
Tom Marianer <tommaria@mail.tau.ac.il>
2020-12-23 11:08:39 +0200

    added plot_tools_multi_scale.py, which allowes interactive plots with multi-scale spectrograms added process_injected and process_injected_ccsn notebooks - notebooks for post-processing software injections

--- local/tools/params.py
+++ local/tools/params.py
@@ -5,6 +5,12 @@ parameters file for local
 
 from pathlib import Path
 
+scales = [0.5, 1.0, 2.0]
+input_shape = (299, 299)
+
+frange=(10, 2048)
+qrange=(4, 100)
+
 ### model parameters
 
 condition_method = None #'gs_wrap_no_ty'
@@ -13,8 +19,11 @@ model = None #'new/resnet152v2/adadelta/gpu_test_15'
 ### kde parameters
 
 # map grid ranges
-x_range = (-16, 22) 
-y_range = (-25, 21.5)
+# x_range = (-16, 22) 
+# y_range = (-25, 21.5)
+
+x_range = (-11, 24)
+y_range = (-18, 21)
 
 # estimator parameters
 kernel = 'gaussian'
